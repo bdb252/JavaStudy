2a994de0f092216f7e5919ef77ff525d96ebb7b5
bdb252 <hanyilee@naver.com>
2025-03-30 14:25:26 +0900

    3주차과제

---
+++ src/ex17collection/QuArrayList.java
@@ -0,0 +1,85 @@
+package ex17collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Student{
+	private String name;
+	private int age;
+	private String year;
+	public Student(String name, int age, String year) {
+		this.name = name;
+		this.age = age;
+		this.year = year;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getYear() {
+		return year;
+	}
+	@Override
+	public String toString() {
+		return "이름:" + name + ", 나이:" + age + ", 학번" + year;
+	}
+	
+	
+}
+
+public class QuArrayList {
+
+	public static void main(String[] args) {
+
+//		ArrayList<Student> list = new ArrayList<Student>();
+		LinkedList<Student> list = new LinkedList<Student>();
+		
+		//저장할 객체 생성
+		Student st1 = new Student("가길동", 10, "2018");
+		Student st2 = new Student("나길동", 20, "2017");
+		Student st3 = new Student("다길동", 30, "2016");
+		Student st4 = new Student("마길동", 40, "2015");
+		
+		//객체 추가(컬렉션에 저장)
+		list.add(st1);
+		list.add(st2);
+		list.add(st3);
+		list.add(st4);
+			
+		//1.검색할 이름을 입력받음
+		Scanner sc = new Scanner(System.in);
+		System.out.print("검색할 이름을 입력하세요:");
+		String scName = sc.nextLine();
+				
+		//2.확장for문으로 컬렉션 전체를 접근
+		boolean flag = false;
+		for(Student obj : list) {
+			if(scName.equals(obj.getName())) {
+				list.remove(obj);
+				System.out.println("[검색되었습니다]");
+				flag = true;
+				break;
+			}
+		}
+		//3.검색결과 유/무에 따라 
+		//검색결과 있을때…검색된 데이터 삭제
+		//검색결과 없을때...검색결과가 없다고 출력
+		if(flag) {
+			System.out.println("[삭제 후 정보출력]");		
+		}
+		else {
+			System.out.println("[검색 결과가 없습니다]");
+		}
+
+		//4.전체정보 출력
+		Iterator<Student> it = list.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+	}
+
+}
---
+++ src/quiz/QuUpDownGame.java
@@ -0,0 +1,52 @@
+package quiz;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuUpDownGame {
+
+	static Scanner sc = new Scanner(System.in);
+	static Random random = new Random();
+	static int cpu = random.nextInt(100)+1;
+	static int num = 0;
+
+	public static void reGameQuestion() {
+		System.out.println("게임을 다시 시작할까요?\n1.게임재시작, 0:게임종료");
+		int reGame = sc.nextInt();
+		if(reGame == 1) {
+			System.out.println("게임을 재시작합니다.");
+			cpu = random.nextInt(100)+1;
+			System.out.println("생성된 난수:"+cpu);
+			num = 0;
+		}
+		else {
+			System.out.println("감사합니다. 프로그램을 종료합니다.");
+			System.exit(0);
+		}
+	}
+	public static void main(String[] args) {
+
+		System.out.println("생성된 난수:"+cpu);
+		while(num < 7) {
+			System.out.print("1~100사이의 정수를 입력하세요:");
+			int user = sc.nextInt();
+			if(cpu > user) {
+				System.out.println("Up 입니다.");
+				num++;
+			}
+			else if(cpu < user){
+				System.out.println("Down입니다.");
+				num++;
+			}
+			else {
+				System.out.println("정답입니다.\n승리하셨습니다.");
+				reGameQuestion();
+			}
+			if(num == 7) {
+				System.out.println("실패하셨습니다.");
+				reGameQuestion();				
+			}
+		}
+	}
+
+}
